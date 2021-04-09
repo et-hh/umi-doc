ff66684d539e54e69ceb031bcce4fbf5684d6ba3
zhoujie@yiwise.com <zhoujie@yiwise.com>
2021-04-09 11:52:14 +0800

    fix

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const fs = require('fs')
 const docIndexContent = fs.readFileSync(path.resolve(__dirname, './doc/index.tsx'))
 const cssContext = fs.readFileSync(path.resolve(__dirname, './doc/index.css'))
 
-export default function (api) {
+module.exports = function (api) {
   api.modifyRoutes((routes) => {
-    return [{ path: '/componentsPage', component: path.resolve(__dirname, './src/.umi/doc') }, ...routes]
+    return [{ path: '/componentsPage', component: path.resolve(__dirname, '../../src/.umi/doc') }, ...routes]
   })
   api.chainWebpack((config) => {
     config.module.rule('js').use('custom').loader('umi-doc/CustomLoader')
